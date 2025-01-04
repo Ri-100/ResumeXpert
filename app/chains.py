0d133a90c6c6a552a49e3dd1edb5ecfe6a4dc08f
Rishav <104222508+Ri-100@users.noreply.github.com>
2025-01-05 00:54:19 +0530

    Update chains.py

--- app/chains.py
+++ app/chains.py
@@ -1,3 +1,6 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import os
 from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
