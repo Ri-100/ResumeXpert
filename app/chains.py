b795587371acaed7f804dcda8011f58cddb28035
Rishav <104222508+Ri-100@users.noreply.github.com>
2025-01-29 12:47:07 +0530

    Update chains.py

--- app/chains.py
+++ app/chains.py
@@ -14,7 +14,7 @@ class Chain:
         self.llm = ChatGroq(
             temperature=0,
             groq_api_key=st.secrets["GROQ_API_KEY"],
-            model_name="llama-3.2-11b-vision-preview"
+            model_name="llama-3.1-8b-instant"
         )
 
     def extract_jobs(self, cleaned_text):
